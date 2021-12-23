4259e26b91c385cb806f53e3c4da19b44686cd59
ManukumarNM <manunm.jps@gmail.com>
2021-12-23 12:12:14 +0530

    SMTP is added

--- Django_Ecommerce/settings.py
+++ Django_Ecommerce/settings.py
@@ -145,3 +145,10 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     50: 'critical',
 }
+
+#SMTP configuration 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'manunmkumar@gmail.com'
+EMAIL_HOST_PASSWORD = 'manunm.jps@93'
+EMAIL_USE_TLS = True
