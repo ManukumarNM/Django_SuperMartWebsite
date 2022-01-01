7d8ce4f62e48ef6173ff77370d9f674b3f1d1faf
ManukumarNM <manunm.jps@gmail.com>
2022-01-01 11:37:42 +0530

    Product gallery with unlimited images

--- store_app/views.py
+++ store_app/views.py
@@ -4,7 +4,7 @@ from django.core import paginator
 from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from .models import Product, ReviewRating 
+from .models import Product, ReviewRating, ProductGallery
 from category_app.models import Category
 from carts_app.views import _cart_id
 from carts_app.models import Cartitem
@@ -57,11 +57,15 @@ def product_detail(request, category_slug, product_slug):
     # Get the reviews
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
+    # Get the product gallery 
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
         'orderproduct': orderproduct,
         'reviews': reviews,
+        'product_gallery': product_gallery,
     }
     return render(request, 'store/product_detail.html', context)
 
