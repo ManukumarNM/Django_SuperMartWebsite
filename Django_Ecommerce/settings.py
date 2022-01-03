61051a5bd517ebcd3a3b4355daa9b12c6677f152
Manukumar N M <manunm.jps@gmail.com>
2022-01-03 13:29:17 +0530

    Update settings.py

--- Django_Ecommerce/settings.py
+++ Django_Ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['supermart-env.eba-pndxxfmy.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
